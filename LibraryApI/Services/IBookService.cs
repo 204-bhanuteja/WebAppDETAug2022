23944874d182fe2ba49d89b30bfca7bad2f89aa2
bhanu <bhanuprakash123321@gmail.com>
2022-09-15 17:53:09 +0530

    book and bank

---
+++ LibraryApI/Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApI.Services
+{
+    internal class IBookService
+    {
+    }
+}
